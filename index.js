d982e2b579f3b89639e42a80dbdba7f2956dfde2
sabirkazi125 <sabirkazi125@gmail.com>
2025-01-02 09:15:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-let max = 100;
-let min = 50;
+let num = new Date().getDay();
 
-console.log(Math.random() * (max - min) + min);
+switch (num) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wedsday");
+    break;
+  case 4:
+    console.log("Thusday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+
+  default:
+    console.log("hue hue");
+    break;
+}
