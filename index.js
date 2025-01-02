ad61f14da91d42dac8df2e493950ced927cdaf9d
sabirkazi125 <sabirkazi125@gmail.com>
2025-01-02 09:24:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let num = Math.floor(Math.random() * 4);
+let num = Math.floor(Math.random() * 12 + 1);
 console.log(num);
+
+switch (num) {
+  case 12:
+  case 1:
+  case 2:
+    console.log("winter");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Spring");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Summer");
+    break;
+
+  default:
+    console.log("Fall");
+    break;
+}
